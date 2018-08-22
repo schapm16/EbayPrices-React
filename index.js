c897669885d30f507852e300cba84ec0ab9eb5f2
Stephen Chapman <Schapm16@gmail.com>
2018-08-22 00:21:39 -0400

    Changes to Express Server - details follow - - Change to PORT 3001 to accomodate create-react-app dev server - Remove unnecessary urlencoded body-parser middleware.  So far I have used only JSON.

--- index.js
+++ index.js
@@ -4,10 +4,9 @@ const path = require('path');
 
 const express = require('express');
 const app = express();
-const PORT = 3000
+const PORT = 3001;
 
 const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 const ebayAPI = require('./ebayAPI');
