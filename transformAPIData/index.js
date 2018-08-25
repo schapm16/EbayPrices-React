d361f42a573d4489e7220f66b00cb594ee097768
Stephen Chapman <Schapm16@gmail.com>
2018-08-25 01:14:33 -0400

    Incorporate MomentJS to handle dates on server - So far MomentJS is used to format startTimes and endTimes of Ebay listings when transforming data.

--- transformAPIData/index.js
+++ transformAPIData/index.js
@@ -1,3 +1,5 @@
+const moment = require('moment');
+
 const default_shippingCost = '10.50';
 
 function transformListings(listings) {
@@ -9,8 +11,8 @@ function transformListings(listings) {
       viewItemURL: listing.viewItemURL[0],
       shippingCost:(listing.shippingInfo[0].shippingServiceCost) ? parseFloat(listing.shippingInfo[0].shippingServiceCost[0].__value__).toFixed(2) : default_shippingCost,
       price:parseFloat(listing.sellingStatus[0].convertedCurrentPrice[0].__value__).toFixed(2),
-      startTime: listing.listingInfo[0].startTime[0],
-      endTime: listing.listingInfo[0].endTime[0],
+      startTime: moment(listing.listingInfo[0].startTime[0]).format('MM/DD/YYYY HH:mm'),
+      endTime: moment(listing.listingInfo[0].endTime[0]).format('MM/DD/YYYY HH:mm'),
       condition: listing.condition[0].conditionDisplayName[0],
       isMultiVariationListing: listing.isMultiVariationListing[0]
     }
