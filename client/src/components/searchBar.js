7e0f88667b7b30f28ae3e37fa6a942b76a6f33a5
Stephen Chapman <Schapm16@gmail.com>
2018-09-03 01:24:52 -0400

    Refining overall flexbox layout and other styling

--- client/src/components/searchBar.js
+++ client/src/components/searchBar.js
@@ -7,7 +7,7 @@ import './searchBar.css';
 const SearchBar = () => {
   return(
     <div className="searchBar">
-      <Link to="/search" className="material-icons search-icon">search</Link>
+      <Link to="/search" className="material-icons search-icon fs-xlg">search</Link>
       <span className="fs-xlg fw-md">Ebay PriceFinder</span>
     </div>
 
