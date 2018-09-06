276ae3b2e6d2bf3016b8bb739fb1ffc0abcbc6b5
Stephen Chapman <Schapm16@gmail.com>
2018-09-05 21:58:27 -0400

    Fixed a silly mistake - I had a syntax mistake in the spots where I was filtering out code for production environments.

--- ebayAPI/index.js
+++ ebayAPI/index.js
@@ -1,6 +1,6 @@
 const request = require('request-promise-native');
 
-if (!process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV !== "production") {
   const devUtils = require('./devUtils');
   devUtils.logRequests(request);
 }
