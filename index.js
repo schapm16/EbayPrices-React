755e56d3621a3e3c585b4e09897dd336cdd99ed6
Stephen Chapman <Schapm16@gmail.com>
2018-09-05 21:09:08 -0400

    Set app to bind to Heroku process.env.PORT

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const express = require('express');
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
