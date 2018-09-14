90f2a6e7193a3f9b324803119d54e2975f67172e
Stephen Chapman <Schapm16@gmail.com>
2018-09-14 08:54:17 -0400

    Change html attribute to JSX version

--- client/src/components/inputSwitch.js
+++ client/src/components/inputSwitch.js
@@ -9,10 +9,10 @@ const InputSwitch = ({ name, options, switchState, onChange, fontSize='fs-md' })
 
   return (
     <div className={`input-switch text-center ${fontSize}`}>
-      <label for={options.one.value} className={label1Class}>{options.one.display}
+      <label htmlFor={options.one.value} className={label1Class}>{options.one.display}
         <input type="radio" id={options.one.value} name={name} value={options.one.value} onChange={onChange}/>
       </label>
-      <label for={options.two.value} className={label2Class}>{options.two.display}
+      <label htmlFor={options.two.value} className={label2Class}>{options.two.display}
         <input type="radio" id={options.two.value} name={name} value={options.two.value} onChange={onChange}/>
       </label>
     </div>
